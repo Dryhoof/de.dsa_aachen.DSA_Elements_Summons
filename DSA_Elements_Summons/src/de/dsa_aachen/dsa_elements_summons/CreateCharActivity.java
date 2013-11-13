808df0fd7463cd5e279c88bbe17bebaef8301ee3
Aton Suntas <Aton Suntas@Suntas-Home>
2013-11-13 21:56:27 +0100

    Charackter config complete!

--- DSA_Elements_Summons/src/de/dsa_aachen/dsa_elements_summons/CreateCharActivity.java
+++ DSA_Elements_Summons/src/de/dsa_aachen/dsa_elements_summons/CreateCharActivity.java
@@ -2,10 +2,8 @@ package de.dsa_aachen.dsa_elements_summons;
 
 import android.app.Activity;
 import android.content.ContentValues;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQuery;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.CheckBox;
@@ -55,29 +53,38 @@ public class CreateCharActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.create_char_activity);
 		
-		//SharedPreferences settings = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
-		
 		SQLiteDatabase Database = DB.getReadableDatabase();
 		Cursor query = Database.query(false, "Characters", null, null, null, null, null, "id ASC", null);
 		if(query.getCount() != 0){
 			query.moveToFirst();
-			//int test = query.getColumnCount();
-			//boolean charTalentedFire = settings.getBoolean("charTalentedFire", false);
-	
-			/*boolean talentedFire = query.getInt(dbField.talentedFire.intValue)>0;
-		    final CheckBox createCharCheckBoxTalentedFire = (CheckBox) findViewById(R.id.createCharCheckBoxTalentedFire);
-		    createCharCheckBoxTalentedFire.setChecked(talentedFire);
-	
-		    //String charCharacterName = settings.getString("CharCharacterName", null);
-			String characterName = query.getString(dbField.characterName.intValue);
-		    final TextView createCharEditCharacterName = (TextView) findViewById(R.id.createCharEditCharacterName);
-		    createCharEditCharacterName.setText(characterName);*/
-		    
-		    //int CharStatCourage = settings.getInt("CharStatCourage", 0);
-			/*System.out.println("query.getCount()" + " " +query.getCount());
-			System.out.println("query.getColumnCount()" + " " +query.getColumnCount());
-			System.out.println("dbField.statCourage.intValue" +" " + dbField.statCourage.intValue);
-			System.out.println("query.getPosition()" +" " + query.getPosition());*/
+
+			setEditTextString(query, dbField.characterName.intValue, R.id.createCharEditCharacterName);
+			setEditTextInt(query, dbField.statCourage.intValue, R.id.createCharEditStatCourage);
+			setEditTextInt(query, dbField.statWisdom.intValue, R.id.createCharEditStatWisdom);
+			setEditTextInt(query, dbField.statCharisma.intValue, R.id.createCharEditStatCharisma);
+			setEditTextInt(query, dbField.statIntuition.intValue, R.id.createCharEditStatIntuition);
+			setEditTextInt(query, dbField.talentCallElementalServant.intValue, R.id.createCharEditTalentCallElementalServant);
+			setEditTextInt(query, dbField.talentCallDjinn.intValue, R.id.createCharEditTalentCallDjinn);
+			setEditTextInt(query, dbField.talentCallMasterOfElement.intValue, R.id.createCharEditTalentCallMasterOfElement);
+			setCheckBox(query, dbField.talentedFire.intValue, R.id.createCharCheckBoxTalentedFire);
+			setCheckBox(query, dbField.talentedWater.intValue, R.id.createCharCheckBoxTalentedWater);
+			setCheckBox(query, dbField.talentedLife.intValue, R.id.createCharCheckBoxTalentedLife);
+			setCheckBox(query, dbField.talentedIce.intValue, R.id.createCharCheckBoxTalentedIce);
+			setCheckBox(query, dbField.talentedStone.intValue, R.id.createCharCheckBoxTalentedStone);
+			setCheckBox(query, dbField.talentedAir.intValue, R.id.createCharCheckBoxTalentedAir);
+			setEditTextInt(query, dbField.talentedDemonic.intValue, R.id.createCharEditTalentedDemonic);
+			setCheckBox(query, dbField.knowledgeFire.intValue, R.id.createCharCheckBoxKnowledgeFire);
+			setCheckBox(query, dbField.knowledgeWater.intValue, R.id.createCharCheckBoxKnowledgeWater);
+			setCheckBox(query, dbField.knowledgeLife.intValue, R.id.createCharCheckBoxKnowledgeLife);
+			setCheckBox(query, dbField.knowledgeIce.intValue, R.id.createCharCheckBoxKnowledgeIce);
+			setCheckBox(query, dbField.knowledgeStone.intValue, R.id.createCharCheckBoxKnowledgeStone);
+			setCheckBox(query, dbField.knowledgeAir.intValue, R.id.createCharCheckBoxKnowledgeAir);
+			setEditTextInt(query, dbField.knowledgeDemonic.intValue, R.id.createCharEditKnowledgeDemonic);
+			setCheckBox(query, dbField.affinityToElementals.intValue, R.id.createCharCheckBoxAffinityToElementals);
+			setCheckBox(query, dbField.demonicCovenant.intValue, R.id.createCharCheckBoxDemonicCovenant);
+			setCheckBox(query, dbField.cloakedAura.intValue, R.id.createCharCheckBoxCloakedAura);
+			setEditTextInt(query, dbField.weakPresence.intValue, R.id.createCharEditWeakPresence);
+			setEditTextInt(query, dbField.strengthOfStigma.intValue, R.id.createCharEditStrengthOfStigma);
 			
 			String characterName = query.getString(dbField.characterName.intValue);
 		    final TextView createCharEditCharacterName = (TextView) findViewById(R.id.createCharEditCharacterName);
@@ -106,156 +113,35 @@ public class CreateCharActivity extends Activity {
 		
         SQLiteDatabase Database = DB.getWritableDatabase();
         ContentValues values = new ContentValues();
-        //values.put(dbField.id.stringValue,);
         
-
 		values.put(dbField.id.stringValue,1);
-		
-        final TextView createCharEditCharacterName = (TextView) findViewById(R.id.createCharEditCharacterName);
-        String createCharEditCharacterNameString = createCharEditCharacterName.getText().toString();
-        if(createCharEditCharacterNameString != null){
-        	values.put(dbField.characterName.stringValue,createCharEditCharacterNameString);
-        }else{
-        		values.put(dbField.characterName.stringValue,"");
-        }
-        
-        final TextView createCharEditStatCourage = (TextView) findViewById(R.id.createCharEditStatCourage);
-        String createCharEditStatCourageString = createCharEditStatCourage.getText().toString();
-        int createCharEditStatCourageInt;
-        if(createCharEditStatCourageString == ""){
-        	createCharEditStatCourageInt = 0;
-        }else{
-            createCharEditStatCourageInt =  Integer.parseInt(createCharEditStatCourageString);
-        }
-        values.put(dbField.statCourage.stringValue,createCharEditStatCourageInt);
-
-        final TextView createCharEditStatWisdom = (TextView) findViewById(R.id.createCharEditStatWisdom);
-        String createCharEditStatWisdomString = createCharEditStatWisdom.getText().toString();
-        int createCharEditStatWisdomInt;
-        if(createCharEditStatWisdomString == ""){
-        	createCharEditStatWisdomInt = 0;
-        }else{
-            createCharEditStatWisdomInt =  Integer.parseInt(createCharEditStatWisdomString);
-        }
-        values.put(dbField.statWisdom.stringValue,createCharEditStatWisdomInt);
-        
-        final TextView createCharEditStatCharisma = (TextView) findViewById(R.id.createCharEditStatCharisma);
-        String createCharEditStatCharismaString = createCharEditStatCharisma.getText().toString();
-        int createCharEditStatCharismaInt;
-        if(createCharEditStatCharismaString == ""){
-        	createCharEditStatCharismaInt = 0;
-        }else{
-            createCharEditStatCharismaInt =  Integer.parseInt(createCharEditStatCharismaString);
-        }
-        values.put(dbField.statCharisma.stringValue,createCharEditStatCharismaInt);
-        
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-        values.put(dbField.statIntuition.stringValue,createCharEditStatIntuitionInt);
-        
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-        values.put(dbField.talentCallElementalServant.stringValue,0);
-
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-        values.put(dbField.talentCallDjinn.stringValue,0);
-
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-        values.put(dbField.talentCallMasterOfElement.stringValue,0);
-        
-        
-        final CheckBox createCharCheckBoxTelendetFire = (CheckBox) findViewById(R.id.createCharCheckBoxTalentedFire);
-        values.put(dbField.talentedFire.stringValue,createCharCheckBoxTelendetFire.isChecked());
-        
-        values.put(dbField.talentedWater.stringValue,0);
-        values.put(dbField.talentedLife.stringValue,0);
-        values.put(dbField.talentedIce.stringValue,0);
-        values.put(dbField.talentedStone.stringValue,0);
-        values.put(dbField.talentedAir.stringValue,0);
-
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-        values.put(dbField.talentedDemonic.stringValue,0);
-        
-		values.put(dbField.knowledgeFire.stringValue,0);
-		values.put(dbField.knowledgeWater.stringValue,0);
-		values.put(dbField.knowledgeLife.stringValue,0);
-		values.put(dbField.knowledgeIce.stringValue,0);
-		values.put(dbField.knowledgeStone.stringValue,0);
-		values.put(dbField.knowledgeAir.stringValue,0);
-
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-		values.put(dbField.knowledgeDemonic.stringValue,0);
-		
-		values.put(dbField.affinityToElementals.stringValue,0);
-		values.put(dbField.demonicCovenant.stringValue,0);
-		values.put(dbField.cloakedAura.stringValue,0);
-
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-		values.put(dbField.weakPresence.stringValue,0);
-
-        //TODO
-        final TextView createCharEditStatIntuition = (TextView) findViewById(R.id.createCharEditStatIntuition);
-        String createCharEditStatIntuitionString = createCharEditStatIntuition.getText().toString();
-        int createCharEditStatIntuitionInt;
-        if(createCharEditStatIntuitionString == ""){
-        	createCharEditStatIntuitionInt = 0;
-        }else{
-            createCharEditStatIntuitionInt =  Integer.parseInt(createCharEditStatIntuitionString);
-        }
-		values.put(dbField.strengthOfStigma.stringValue,0);
+		values.put(dbField.characterName.stringValue,getFormElementString(R.id.createCharEditCharacterName));
+		values.put(dbField.statCourage.stringValue,getFormElementInt(R.id.createCharEditStatCourage));
+		values.put(dbField.statWisdom.stringValue,getFormElementInt(R.id.createCharEditStatWisdom));
+		values.put(dbField.statCharisma.stringValue,getFormElementInt(R.id.createCharEditStatCharisma));
+		values.put(dbField.statIntuition.stringValue,getFormElementInt(R.id.createCharEditStatIntuition));
+		values.put(dbField.talentCallElementalServant.stringValue,getFormElementInt(R.id.createCharEditTalentCallElementalServant));
+		values.put(dbField.talentCallDjinn.stringValue,getFormElementInt(R.id.createCharEditTalentCallDjinn));
+		values.put(dbField.talentCallMasterOfElement.stringValue,getFormElementInt(R.id.createCharEditTalentCallMasterOfElement));
+		values.put(dbField.talentedFire.stringValue,getFormElementBoolean(R.id.createCharCheckBoxTalentedFire));
+		values.put(dbField.talentedWater.stringValue,getFormElementBoolean(R.id.createCharCheckBoxTalentedWater));
+		values.put(dbField.talentedLife.stringValue,getFormElementBoolean(R.id.createCharCheckBoxTalentedLife));
+		values.put(dbField.talentedIce.stringValue,getFormElementBoolean(R.id.createCharCheckBoxTalentedIce));
+		values.put(dbField.talentedStone.stringValue,getFormElementBoolean(R.id.createCharCheckBoxTalentedStone));
+		values.put(dbField.talentedAir.stringValue,getFormElementBoolean(R.id.createCharCheckBoxTalentedAir));
+		values.put(dbField.talentedDemonic.stringValue,getFormElementInt(R.id.createCharEditTalentedDemonic));
+		values.put(dbField.knowledgeFire.stringValue,getFormElementBoolean(R.id.createCharCheckBoxKnowledgeFire));
+		values.put(dbField.knowledgeWater.stringValue,getFormElementBoolean(R.id.createCharCheckBoxKnowledgeWater));
+		values.put(dbField.knowledgeLife.stringValue,getFormElementBoolean(R.id.createCharCheckBoxKnowledgeLife));
+		values.put(dbField.knowledgeIce.stringValue,getFormElementBoolean(R.id.createCharCheckBoxKnowledgeIce));
+		values.put(dbField.knowledgeStone.stringValue,getFormElementBoolean(R.id.createCharCheckBoxKnowledgeStone));
+		values.put(dbField.knowledgeAir.stringValue,getFormElementBoolean(R.id.createCharCheckBoxKnowledgeAir));
+		values.put(dbField.knowledgeDemonic.stringValue,getFormElementInt(R.id.createCharEditKnowledgeDemonic));
+		values.put(dbField.affinityToElementals.stringValue,getFormElementBoolean(R.id.createCharCheckBoxAffinityToElementals));
+		values.put(dbField.demonicCovenant.stringValue,getFormElementBoolean(R.id.createCharCheckBoxDemonicCovenant));
+		values.put(dbField.cloakedAura.stringValue,getFormElementBoolean(R.id.createCharCheckBoxCloakedAura));
+		values.put(dbField.weakPresence.stringValue,getFormElementInt(R.id.createCharEditWeakPresence));
+		values.put(dbField.strengthOfStigma.stringValue,getFormElementInt(R.id.createCharEditStrengthOfStigma));
 		
 		if(queryCount == 1){
 			Database.update("Characters", values, "id = '1'", null);
@@ -263,21 +149,45 @@ public class CreateCharActivity extends Activity {
         	Database.insert("Characters", "id", values);
 		}
 		Database.close();
-		//SharedPreferences settings = getSharedPreferences(PREFS_NAME,MODE_PRIVATE);
-		//SharedPreferences.Editor editor = settings.edit();
-		
-		
-		//editor.putString("CharCharacterName", createCharEditCharacterNameString);
-		
-		//final TextView createCharEditStatCourage = (TextView) findViewById(R.id.createCharEditStatCourage);
-		//int createCharEditStatCourageInt =  Integer.parseInt(createCharEditStatCourage.getText().toString());
-		//editor.putInt("CharStatCourage", createCharEditStatCourageInt);
-		
-		
-		// Commit the edits!
-		//editor.commit();
 	}
+	private boolean getFormElementBoolean(int Rid){
+		final CheckBox checkBox = (CheckBox) findViewById(Rid);
+		return checkBox.isChecked();
+	}
+	private int getFormElementInt(int Rid){
+		String RidTextViewString = getFormElementString(Rid);
+		if(RidTextViewString == ""){
+        	return(0);
+        }else{
+            return(Integer.parseInt(RidTextViewString));
+        }
+	}
+	private String getFormElementString(int Rid){
+		final TextView RidTextView = (TextView) findViewById(Rid);
+		CharSequence sequence = RidTextView.getText();
+		if(sequence.length() == 0){
+			return("");
+		}
+        String RidTextViewString = sequence.toString();
+        return(RidTextViewString);
+	}
+	private void setCheckBox(Cursor cursor, int columnId, int Rid){
+	    boolean bool = cursor.getInt(columnId)>0;
+	    final CheckBox checkBox = (CheckBox) findViewById(Rid);
+	    checkBox.setChecked(bool);
+	}
+	private void setEditTextInt(Cursor cursor, int columnId, int Rid){
+		int Int = cursor.getInt(columnId);
+	    final TextView textView = (TextView) findViewById(Rid);
+	    textView.setText(String.valueOf(Int));
 
+		
+	}
+	private void setEditTextString(Cursor cursor, int columnId, int Rid){
+		String string = cursor.getString(columnId);
+	    final TextView textView = (TextView) findViewById(Rid);
+	    textView.setText(string);
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
